20e4b6239041f7eab9f6f69e5ec85e3b4344cc81
Новиков Денис <dreemkiller69@gmail.com>
2019-06-01 11:52:32 +0300

    Initial commit
    Added initiating file "WallCheker.php" and test data file "data.txt"

---
+++ WallCheker.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shockvvave
+ * Date: 01.06.19
+ * Time: 11:49
+ */
+
+/**
+ * @param $args
+ *
+ */
+function init($args)
+{
+    var_dump($args[1]);
+}
+
+//Initiate
+init($argv);
